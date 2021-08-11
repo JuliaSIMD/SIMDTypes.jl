6bd5d4a97f6ab33b6c2a8f1f1e381168eb6b6d1c
Chris Elrod <elrodc@gmail.com>
2021-08-10 23:49:57 -0400

    Created repo.

--- src/SIMDTypes.jl
+++ src/SIMDTypes.jl
@@ -1,5 +1,19 @@
 module SIMDTypes
 
-# Write your package code here.
+struct Bit; data::Bool; end # Dummy for Ptr
+# @inline Base.convert(::Type{Bool}, b::Bit) = getfield(b, :data)
+
+const FloatingTypes = Union{Float16,Float32,Float64}
+const SignedHW = Union{Int8,Int16,Int32,Int64}
+const UnsignedHW = Union{UInt8,UInt16,UInt32,UInt64}
+const IntegerTypesHW = Union{SignedHW,UnsignedHW}
+const IntegerTypes = Union{StaticInt,IntegerTypesHW}
+
+const NativeTypesExceptBitandFloat16 = Union{Bool,HWReal}
+const NativeTypesExceptBit = Union{Bool,HWReal,Float16}
+const NativeTypesExceptFloat16 = Union{Bool,HWReal,Bit}
+const NativeTypes = Union{NativeTypesExceptBit, Bit}
+
+const _Vec{W,T<:Number} = NTuple{W,Core.VecElement{T}}
 
 end
