b990b6506e2d7557263d726c5bf26a19dadbf497
Chris Elrod <elrodc@gmail.com>
2023-01-25 17:08:41 -0500

    max-methods=1

--- src/SIMDTypes.jl
+++ src/SIMDTypes.jl
@@ -1,4 +1,8 @@
 module SIMDTypes
+if isdefined(Base, :Experimental) &&
+   isdefined(Base.Experimental, Symbol("@max_methods"))
+    @eval Base.Experimental.@max_methods 1
+end
 
 # using Static: StaticInt
 
